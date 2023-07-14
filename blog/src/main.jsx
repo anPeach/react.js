ab90bc900744368d72eb071e6a0d32c649ec176f
anPeach <nastyapisarenko2001@gmail.com>
2023-07-14 12:58:12 +0300

    Set up React.js project

---
+++ blog/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from './App.jsx'
+import {Auth, Profile} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {path: '/auth', element: <Auth/>},
+  {path: '/profile', element: <Profile/>},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
